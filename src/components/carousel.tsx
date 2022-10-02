fa4673be5698595b869282b7443ebaa52e823541
Sowad <sowad.dev@gmail.com>
2022-10-02 08:15:26 +0600

    updated hero carousel - swiper, next/image

--- src/components/carousel.tsx
+++ src/components/carousel.tsx
@@ -1,6 +1,9 @@
+import Image from "next/image";
 import { FaLongArrowAltLeft, FaLongArrowAltRight } from "react-icons/fa";
-import { Carousel as ReactCarousel } from "react-responsive-carousel";
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Navigation } from "swiper";
+import "swiper/css";
+import "swiper/css/navigation";
+import { Swiper, SwiperSlide } from "swiper/react";
 
 const images = [
   "/assets/banner/1.jpg",
@@ -10,33 +13,47 @@ const images = [
 
 const Carousel: React.FC = () => {
   return (
-    <ReactCarousel
-      infiniteLoop={true}
-      autoPlay={true}
-      swipeable={true}
-      showThumbs={false}
-      showIndicators={false}
-      showStatus={false}
-      renderArrowNext={(onClick) => <Arrow onClick={onClick} />}
-      renderArrowPrev={(onClick) => <Arrow onClick={onClick} prev />}
-    >
-      {images.map((img, idx) => (
-        // eslint-disable-next-line @next/next/no-img-element
-        <img key={idx} src={img} alt="banner" />
-      ))}
-    </ReactCarousel>
+    <div className="relative">
+      <Swiper
+        modules={[Navigation]}
+        loop={true}
+        navigation={{
+          prevEl: ".hero-carousel-navigation-prev",
+          nextEl: ".hero-carousel-navigation-next",
+          disabledClass: "text-gray-500",
+        }}
+      >
+        {images.map((item, key) => (
+          <SwiperSlide key={key}>
+            <div className="relative h-full w-full">
+              <Image
+                src={item}
+                alt="banner"
+                width="100%"
+                height="42%"
+                layout="responsive"
+                objectFit="cover"
+              />
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+      <Arrow prev />
+      <Arrow />
+    </div>
   );
 };
 
-const Arrow = ({ onClick, prev }: { onClick: () => void; prev?: boolean }) => (
+const Arrow = ({ prev }: { prev?: boolean }) => (
   <div
     className={`absolute top-0 bottom-0 z-10 ${
       prev ? "left-0" : "right-0"
     } flex items-center`}
   >
     <button
-      onClick={onClick}
-      className="bg-primary-500 p-1 hover:bg-primary-600 focus:outline-none focus:ring-4 focus:ring-primary-900 sm:p-3 md:p-4"
+      className={`${
+        prev ? "hero-carousel-navigation-prev" : "hero-carousel-navigation-next"
+      } bg-primary-500 p-1 hover:bg-primary-600 focus:outline-none focus:ring-4 focus:ring-primary-900 sm:p-3 md:p-4`}
     >
       {prev ? (
         <FaLongArrowAltLeft className="text-sm sm:text-lg md:text-xl lg:text-3xl" />

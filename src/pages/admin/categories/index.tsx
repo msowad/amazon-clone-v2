d82467cac3990d570467918ee9a29223f17144fe
Sowad <sowad.dev@gmail.com>
2022-10-16 16:14:27 +0600

    Fixed profile menu overlapping by table

--- src/pages/admin/categories/index.tsx
+++ src/pages/admin/categories/index.tsx
@@ -41,7 +41,7 @@ const Index = () => {
           icon={<MdAddCircle size={20} />}
         />
       </div>
-      <div className="relative my-10 overflow-x-auto shadow-md sm:rounded-lg">
+      <div className="my-10 overflow-x-auto shadow-md sm:rounded-lg">
         <table className="w-full text-left text-sm text-gray-500 dark:text-gray-400">
           <thead className="bg-gray-50 text-xs uppercase text-gray-700 dark:bg-gray-700 dark:text-gray-400">
             {table.getHeaderGroups().map((headerGroup) => (
